624ada90d35743db064747ba00c67bf93273eb63
Lyudmila <milo4kamoya@gmail.com>
2021-11-05 13:18:28 -0700

    new fritsDB created and data added

---
+++ app.js
@@ -0,0 +1,52 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'fruits';
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+// Use connect method to connect to the Server
+client.connect(function (err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  insertDocuments(db, function () {
+    client.close();
+  });
+});
+
+const insertDocuments = function (db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Insert some documents
+  collection.insertMany([
+    {
+      name: "Apple",
+      score: 8,
+      review: "Great fruit"
+    },
+    {
+      name: "Orange",
+      score: 5,
+      review: "Kinda sour"
+    },
+    {
+      name: "Banana",
+      score: 10,
+      review: "The greatest stuff"
+    }
+  ], function (err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.insertedCount);
+    assert.equal(3, Object.keys(result.insertedIds).length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
